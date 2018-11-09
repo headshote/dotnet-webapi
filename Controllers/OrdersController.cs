3fe158d9ab05663dbb6c1f2e498cffc637ba757b
Oleksii Tkachenko <Oleksii_Tkachenko@epam.com>
2018-11-09 10:43:33 +0200

    put method + draft for delete method

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -6,6 +6,9 @@ using System.Web.Http;
 using WebApplicationExercise.Core;
 using WebApplicationExercise.Models;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace WebApplicationExercise.Controllers
@@ -16,12 +19,14 @@ namespace WebApplicationExercise.Controllers
         private MainDataContext _dataContext = new MainDataContext();
         private CustomerManager _customerManager = new CustomerManager();
 
+        // GET: api/Orders/5
         [HttpGet]
-        public Order GetOrder(Guid orderId)
+        public Order GetOrder(Guid id)
         {
-            return _dataContext.Orders.Include(o => o.Products).Single(o => o.Id == orderId);
+            return _dataContext.Orders.Include(o => o.Products).Single(o => o.Id == id);
         }
 
+        // GET: api/Orders
         [HttpGet]
         public IEnumerable<Order> GetOrders(DateTime? from = null, DateTime? to = null, string customerName = null)
         {
@@ -40,6 +45,7 @@ namespace WebApplicationExercise.Controllers
             return orders.Where(o => _customerManager.IsCustomerVisible(o.Customer));
         }
 
+        // POST: api/Orders/5
         [HttpPost]
         public async Task<IHttpActionResult> SaveOrder([FromBody]Order order)
         {
@@ -49,6 +55,56 @@ namespace WebApplicationExercise.Controllers
             }
 
             _dataContext.Orders.Add(order);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(order);
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateOrder(Guid id, Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+            
+            _dataContext.Set<Order>().AddOrUpdate(order);
+
+            try
+            {
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Orders/5
+        public async Task<IHttpActionResult> DeleteOrder(Guid id)
+        {
+            Order order = await _dataContext.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.Orders.Remove(order);
             await _dataContext.SaveChangesAsync();
 
             return Ok(order);
@@ -71,6 +127,11 @@ namespace WebApplicationExercise.Controllers
         private IEnumerable<Order> FilterByDate(IEnumerable<Order> orders, DateTime from, DateTime to)
         {
             return orders.Where(o => o.CreatedDate >= from && o.CreatedDate < to);
+        }
+
+        private bool OrderExists(Guid id)
+        {
+            return _dataContext.Orders.Count(e => e.Id == id) > 0;
         }
     }
 }
